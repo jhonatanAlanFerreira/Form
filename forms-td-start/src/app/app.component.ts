49b96f1cd0d1855f79250e868d5e74c3cc093cd3
jhonatan <jhonatanalanb@gmail.com>
2019-06-15 12:13:56 -0300

    Alterado

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor( private fb: FormBuilder){}
+
+  formulario: FormGroup;
+
+ngOnInit(){
+  this.formulario = this.fb.group({
+    vc_nome: ['',Validators.required],
+    email: this.fb.group({
+      vc_email: ['', Validators.required],
+      ch_checked: [false, Validators.requiredTrue]
+    })
+  });
+
+}
+
+submited(){
+  console.log("Foi enviado");
+}
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
