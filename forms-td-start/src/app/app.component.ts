7c136e3a392ea68a55d9c3d5f96f6603b0642c64
jhonatan <jhonatanalanb@gmail.com>
2019-06-29 19:34:17 -0300

    Adicionado X

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -15,6 +15,12 @@ export class AppComponent implements OnInit {
     {nome: 'bertini',idade: 43, teste:"testando"},
     {nome: 'ferreira',idade: 56, teste:"testando"},
     {nome: 'jessica',idade: 56, teste:"testando"},
+    {nome: 'carolina',idade: 80, teste:"testando"},
+    {nome: 'jhonatan',idade: 25, teste:"testando"},
+    {nome: 'alan',idade: 34, teste:"testando"},
+    {nome: 'bertini',idade: 43, teste:"testando"},
+    {nome: 'ferreira',idade: 56, teste:"testando"},
+    {nome: 'jessica',idade: 56, teste:"testando"},
     {nome: 'carolina',idade: 80, teste:"testando"}
   ]
 
--- forms-td-start/src/app/select/select.component.ts
+++ forms-td-start/src/app/select/select.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, HostListener } from '@angular/core';
-import { ConsoleReporter } from 'jasmine';
 
 @Component({
   selector: 'app-select',
