848bb50b4571c6ff65b2ddc089ee856e760eab6e
jhonatan <jhonatanalanb@gmail.com>
2019-06-29 17:11:40 -0300

    Editando

--- forms-td-start/src/app/select/select.component.ts
+++ forms-td-start/src/app/select/select.component.ts
@@ -10,17 +10,31 @@ export class SelectComponent implements OnInit {
   constructor() { }
 
   @Input() dados = [];
+  @Input() titulos = [];
   @Input() colunas = [];
   @Input() idUnica = 'sr_id';
+  @Input() nome = 'vc_nome';
 
+  colunasEspecificadas = false;
   valores = [];
+  open = false;
+  value = 'Selecione...'
+  valueId = null;
 
-  selecionar(dado){
-    console.log(dado);
+  selecionar(selecionado:any){
+    console.log(selecionado);
+    this.value = selecionado[this.idUnica];
+    this.valueId = selecionado[this.nome];
+    this.open = false;
   }
 
   ngOnInit() {
+
+    if(this.colunas.length > 0){
+      this.colunasEspecificadas = true;
+    }else
     if(this.dados.length > 0) for(let dado in this.dados[0]) this.valores.push(dado);
+
   }
 
 }
